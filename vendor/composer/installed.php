f3d11a6680eb9c699b9df11718e4aac8e58d3ade
Cibirsk <stlv@gmx.com>
2022-02-22 01:15:25 +0100

    none

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e5b2a30e2c4814bcc9d8c233ed1152d099cde636',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e5b2a30e2c4814bcc9d8c233ed1152d099cde636',
+            'dev_requirement' => false,
+        ),
+        'symfony/polyfill-ctype' => array(
+            'pretty_version' => 'v1.24.0',
+            'version' => '1.24.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/polyfill-ctype',
+            'aliases' => array(),
+            'reference' => '30885182c981ab175d4d034db0f6f469898070ab',
+            'dev_requirement' => false,
+        ),
+        'symfony/yaml' => array(
+            'pretty_version' => 'v6.0.3',
+            'version' => '6.0.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/yaml',
+            'aliases' => array(),
+            'reference' => 'e77f3ea0b21141d771d4a5655faa54f692b34af5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ vendor/test.php
@@ -0,0 +1,5 @@
+<?php
+
+use Symfony\Component\Yaml\Yaml;
+
+$value = Yaml::parse("foo: bar");
