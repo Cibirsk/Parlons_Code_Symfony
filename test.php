d398ca7176f8abd10a766384d55af9563c5e74f4
Cibirsk <stlv@gmx.com>
2022-02-23 07:01:58 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,31 +1,9 @@
 <?php
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Yaml\Yaml;
 
-$value = Yaml::parse("Hello world !");
+$value = Yaml::parse("1e+3");
 
-echo $value;
-
-/*
-use Cocur\Slugify\Slugify;
-
-$slugify = new Slugify();
-echo $slugify->slugify('Hello World!'); // hello-world
-*/
-class Text{
-
-    /**
-     * @param $chiffre le chiffre à préfixer par des 0 si < 10
-     * @return string
-     */
-    public static function withZero($chiffre){
-        if($chiffre < 10){
-            return '0' . $chiffre;
-        }else{
-            return $chiffre;
-        }
-    }
-
-}
-echo Text::withZero(7);
+var_dump($value);
