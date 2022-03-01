9bb668f88425b89ad7419e41e35ff805e7f8cc94
Cibirsk <stlv@gmx.com>
2022-03-01 17:47:22 +0100

    none

--- hello-city/src/Controller/HomeController.php
+++ hello-city/src/Controller/HomeController.php
@@ -8,6 +8,19 @@ class HomeController
 {
     public function index()
     {
-        return new Response('<h1>Hello from France</h1>');
+        return new Response(<<<TEXT
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Hello-city</title>
+        </head>
+        <body>
+        <h1>Hello Paris !</h1> 
+        </body>
+        </html>
+        TEXT);
     }
 }
