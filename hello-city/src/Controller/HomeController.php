879a15012fa11a961121c064eb620dac8ad75a85
Cibirsk <stlv@gmx.com>
2022-03-01 07:04:24 +0100

    Update HomeController.php

--- hello-city/src/Controller/HomeController.php
+++ hello-city/src/Controller/HomeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class HomeController
 {
-
+    public function index()
+    {
+        return new Response('<h1>Hello from France</h1>');
+    }
 }
