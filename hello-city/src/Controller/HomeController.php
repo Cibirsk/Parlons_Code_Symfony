e6cb78ef93835c2e16f947ec7b65c4620b4dcc92
Cibirsk <stlv@gmx.com>
2022-03-01 17:59:37 +0100

    Update HomeController.php

--- hello-city/src/Controller/HomeController.php
+++ hello-city/src/Controller/HomeController.php
@@ -8,6 +8,8 @@ class HomeController
 {
     public function index()
     {
+        $name = $_GET['name'];
+
         return new Response(<<<TEXT
         <!DOCTYPE html>
         <html lang="en">
@@ -18,7 +20,7 @@ class HomeController
             <title>Hello-city</title>
         </head>
         <body>
-        <h1>Hello Paris !</h1> 
+        <h1>Hello $name !</h1> 
         </body>
         </html>
         TEXT);
